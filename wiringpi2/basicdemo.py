1499e5f4ca160da15f7c7aa7f6e792c3a1e27867
Brian Dorey <sales@abelectronics.co.uk>
2013-05-08 15:36:36 +0100

    bug fix on pin numbers

--- wiringpi2/basicdemo.py
+++ wiringpi2/basicdemo.py
@@ -6,7 +6,7 @@ import wiringpi2
 pin_base = 65
 i2c_addr = 0x20
 i2c_addr_2 = 0x21
-#pins = [65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80]
+
 
 wiringpi2.wiringPiSetup()
 wiringpi2.mcp23017Setup(pin_base,i2c_addr)
@@ -14,7 +14,7 @@ wiringpi2.mcp23017Setup(pin_base+16,i2c_addr_2)
 
 #for pin in pins:
 while True:
-	for pin in range(65,97):
+	for pin in range(65,96):
 		wiringpi2.pinMode(pin,1)
 		wiringpi2.digitalWrite(pin,1)
 		wiringpi2.delay(100)
