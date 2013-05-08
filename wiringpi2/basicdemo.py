7333d9b9e04746bbfb146257cb13630e9b06e0b6
Brian Dorey <sales@abelectronics.co.uk>
2013-05-08 15:33:17 +0100

    Added demos for Wiringpi2 python

---
+++ wiringpi2/basicdemo.py
@@ -0,0 +1,21 @@
+# Turns on and off each pin in turn of an mcp23017 on address 0x20 and 0x21  ( AB Electronics IO Pi )
+# run using sudo python basicdemo.py
+
+import wiringpi2
+
+pin_base = 65
+i2c_addr = 0x20
+i2c_addr_2 = 0x21
+#pins = [65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80]
+
+wiringpi2.wiringPiSetup()
+wiringpi2.mcp23017Setup(pin_base,i2c_addr)
+wiringpi2.mcp23017Setup(pin_base+16,i2c_addr_2)
+
+#for pin in pins:
+while True:
+	for pin in range(65,97):
+		wiringpi2.pinMode(pin,1)
+		wiringpi2.digitalWrite(pin,1)
+		wiringpi2.delay(100)
+		wiringpi2.digitalWrite(pin,0)
---
+++ wiringpi2/singlepin.py
@@ -0,0 +1,21 @@
+# Single Pin control of IO Pi board mcp23017 on address 0x20 and 0x21  ( AB Electronics IO Pi )
+# run using sudo python singlepin.py
+import wiringpi2
+
+pin_base = 65
+i2c_addr = 0x20
+i2c_addr_2 = 0x21
+
+wiringpi2.wiringPiSetup()
+wiringpi2.mcp23017Setup(pin_base,i2c_addr)
+wiringpi2.mcp23017Setup(pin_base+16,i2c_addr_2)
+
+# reset all pins as outputs and turn off
+for pin in range(65,96):
+	wiringpi2.pinMode(pin,1)
+	wiringpi2.digitalWrite(pin,0)
+	
+# turn on pin 96 - IC 2 pin 16 on IO Pi
+wiringpi2.pinMode(96,1)
+wiringpi2.digitalWrite(96,1)
+
